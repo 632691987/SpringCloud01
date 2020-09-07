7eafba9d4768fce887804206d78c5ad25a263c89
Vincent Zhang <v.zhang@aspanetconomy.net>
2020-09-07 02:37:13 +0200

    add config feature

---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope//刷新功能，每次获取都从 server 端获取
+public class ConfigClientController
+{
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")//直接读取 cloud-config-center-3344 module. 等于 http://config-3344.com:3344/master/config-dev.yml
+    public String getConfigInfo()
+    {
+        return configInfo;
+    }
+}
